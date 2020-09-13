9c2d4137f50cba0909f30c670a1161791e23d4a9
avenmia <mavend2@gmail.com>
2020-09-12 20:54:18 -0500

    Adding initial about and education pages

---
+++ website/src/components/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <p>
+        Here is the about page
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'AboutPage',
+    components: {}
+
+}
+</script>
+
+<style>
+</style>
---
+++ website/src/components/EducationPage.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <p>
+        Here is the education page
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'EducationPage',
+    components: {}
+
+}
+</script>
+
+<style>
+</style>
