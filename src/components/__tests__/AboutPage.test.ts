f2a53b0dbfa49d4c55286fb8566460e0b624a180
avenmia <mavend2@gmail.com>
2020-09-15 20:34:52 -0500

    adding unit test dependencies

---
+++ src/components/__tests__/AboutPage.test.ts
@@ -0,0 +1,10 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+
+
+describe("AboutPage.vue", () => {
+    test("Renders props.msg when passed", () => {
+        const msg = "some message";
+
+    })
+})
