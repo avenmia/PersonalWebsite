36dec97a0eecc5d5e89dad1e3de6e729e902776d
avenmia <mavend2@gmail.com>
2020-10-12 20:11:07 -0500

    Updating unit test

--- src/components/__tests__/HomePage.test.ts
+++ src/components/__tests__/HomePage.test.ts
@@ -7,9 +7,8 @@ import HomePage from "../HomePage.vue"
 describe("HomePage.vue", () => {
     
     test("Renders HomePage", () => {
-        const message = "testMessage";
-        const wrapper = shallowMount(HomePage, { propsData: {msg: message}});
-        expect(wrapper.html()).toContain("Welcome to Mike's webpage");
-        expect(wrapper.html()).toContain(message);
+        const version = "v0.1";
+        const wrapper = shallowMount(HomePage, { propsData: {version}});
+        expect(wrapper.html()).toContain(`<h1>Michael Avendano</h1><p>Software Engineer</p><p>${version}</p>`);
     });
 })
