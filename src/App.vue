32ae54220bbb361d9135af1755f70753c48fe4b1
avenmia <mavend2@gmail.com>
2020-10-12 20:06:14 -0500

    Removing vue-router@next

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-<div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/education">Education</router-link>
+<div class="home">
+    <section class="fullpage">
+        <HomePage msg="Welcome to Mike's website" v-bind:version="version" />
+    </section>
+    <section class="fullpage">
+        <AboutPage />
+    </section>
+    <section class="fullpage">
+        <EducationPage />
+    </section>
 </div>
-<router-view />
 </template>
+
+<script lang="ts">
+import {
+    defineComponent
+} from 'vue';
+import HomePage from '@/components/HomePage.vue'; // @ is an alias to /src
+import AboutPage from '@/components/AboutPage.vue'; // @ is an alias to /src
+import EducationPage from '@/components/EducationPage.vue'; // @ is an alias to /src
+import * as pjson from "../package.json"
+
+console.log(pjson.version);
+
+export default defineComponent({
+    name: 'Home',
+    components: {
+        HomePage,
+        AboutPage,
+        EducationPage
+    },
+    data() {
+        return {
+            version: pjson.version
+        }
+    }
+});
+</script>
 
 <style>
 #app {
@@ -27,5 +58,10 @@
 
 #nav a.router-link-exact-active {
     color: #42b983;
+}
+
+.fullpage {
+    height: 100vh;
+    width: 100%;
 }
 </style>
