cd549c7d74c5b15b64bd6677d7b88b1e14492d34
avenmia <mavend2@gmail.com>
2020-10-20 08:03:31 -0500

    removing default margin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <div class="banner"></div>
     <section class="fullpage">
       <HomeSection
         :version="version"
@@ -37,12 +38,28 @@ export default defineComponent({
 </script>
 
 <style>
+
+* {
+  margin: 0;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+}
+
+.banner {
+  height: 50%;
+  width: 100%;
+  max-width: 100%;
+  background-image: url("./assets/self.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: fill;
+  margin: 0px;
 }
 
 #nav {
