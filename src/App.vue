8ae7f62094092b4500893be3e1f6f17d4653250d
avenmia <mavend2@gmail.com>
2020-10-21 21:57:01 -0500

    Adding initial color

--- src/App.vue
+++ src/App.vue
@@ -43,11 +43,12 @@ export default defineComponent({
 }
 
 #app {
+    background-color: #403A3C;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #E9DACF;
 }
 
 #nav {
