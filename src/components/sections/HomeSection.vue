b0a1bf044d3be33a443d76d7090a6f7292fa6adf
avenmia <mavend2@gmail.com>
2020-11-13 08:40:32 -0600

    Updating styling

--- src/components/sections/HomeSection.vue
+++ src/components/sections/HomeSection.vue
@@ -1,31 +1,30 @@
 <template>
-  <div class="homesection">
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-      <div class="home-container">
-        <div class="home-container-section">
-          <h1>Michael Avendano</h1>
-          <p>Software Engineer</p>
-          <div class="links">
-            <a href="https://www.linkedin.com/in/michael-avendano/">
-              <img
-                src="../../assets/linkedin.png"
-              >
-            </a>
-            <br>
-            <a href="https://github.com/avenmia">
-              <img
-                src="../../assets/github.png"
-              >
-            </a>
-            <br>
-            <a href="https://stackoverflow.com/users/7331107/avenmia">
-              <img
-                src="../../assets/stackoverflow.svg"
-              >
-            </a>
-          </div>
+  <div class="home-section">
+    <div class="home-container">
+      <div class="home-container-section">
+        <h1>Michael Avendano</h1>
+        <p>Software Engineer</p>
+        <div class="links">
+          <a href="https://www.linkedin.com/in/michael-avendano/">
+            <img
+              src="../../assets/linkedin.png"
+            >
+          </a>
+          <br>
+          <a href="https://github.com/avenmia">
+            <img
+              src="../../assets/github.png"
+            >
+          </a>
+          <br>
+          <a href="https://stackoverflow.com/users/7331107/avenmia">
+            <img
+              src="../../assets/stackoverflow.svg"
+            >
+          </a>
         </div>
       </div>
+    </div>
     <p>{{ version }}</p>
   </div>
 </template>
@@ -44,55 +43,34 @@ export default {
 <style scoped>
 
 .home-container-section {
-  margin: 5%;
+  margin-top: 20px;
 }
+
 
 .home-container {
   background: black;
   order: 1;
-  width: 500px;
+  width: auto;
   height: 200px;
   border-radius: 10px;
+  padding: 5vh 10vw
   
 }
 
-.homesection {
+.home-section {
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 100vh;
   background-image: url("../../assets/1440x763ChiSkyline.jpg");
-  height: 100%;
-}
-
-h3 {
-    margin: 40px 0 0;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
-    margin: 5vw;
+  background-size: cover;
 }
 
 .links {
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  height: 50px;
-  margin-top: 30px;
 }
 
-.links:nth-child()
-{
-  position: fixed;
-}
 </style>
