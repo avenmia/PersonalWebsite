98bdadc31af64b2aa6fe63a1f76c03a32d47c838
avenmia <mavend2@gmail.com>
2020-11-13 20:14:30 -0600

    Adding placeholder fetch

---
+++ src/components/sections/ResumeSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="resume">
+    <p>Resume</p>
+    <p>{{ resumeData }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  onMounted,
+  reactive
+} from 'vue';
+
+
+
+export default defineComponent({
+  name: 'Resume',
+  onRenderTriggered(e: any)
+  {
+    debugger;
+  },
+  setup()
+  {
+    const data = reactive({});
+
+    async function fetchData()
+    {
+      return await fetch("https://swapi.dev/api/people/1/", {
+        method: 'get',
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then(res => {return res.json()});
+    }
+    
+    onMounted(async () => 
+    {
+      console.log(await fetchData());
+    })
+  },
+
+
+
+})
+</script>
+
+
+<style scoped>
+
+
+</style>
