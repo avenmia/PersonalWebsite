e1fcb57792ab73140f9cb060bb16006cfabde174
avenmia <mavend2@gmail.com>
2020-11-19 20:39:52 -0600

    updating styles

--- src/components/resume/Skills.vue
+++ src/components/resume/Skills.vue
@@ -2,7 +2,23 @@
 <template>
   <div class="skills">
     <h3>Skills</h3>
-    <p>{{skills}}</p>
+    <!-- <p>Coding Styles: {{ codeStyles }}</p> -->
+    <p>Coding styles</p>
+    <li v-for="style in codeStyles" :key="style">
+    {{ style }}
+    </li>
+    <p>Technologies</p>
+    <li v-for="t in tech" :key="t">
+      {{ t }}
+     </li>
+    <p>Version Control</p>
+    <li v-for="vc in versionControl" :key="vc">
+      {{ vc }}
+     </li>
+    <p>Soft Skills</p>
+    <li v-for="skill in softSkills" :key="skill">
+     {{ skill }}
+     </li>
   </div>  
 </template>
 
@@ -17,7 +33,12 @@ export default defineComponent({
   data()
   {
     console.log(`Data skills: %o`, this.skills)
-    return this.skills;
+    const codeStyles = this.skills["Coding Styles"]
+    const tech = this.skills["Technologies"]
+    const versionControl = this.skills["Version Control"]
+    const softSkills = this.skills["Soft Skills"]
+    console.log("Coding Styles: %o", codeStyles)
+    return {codeStyles, tech, versionControl, softSkills};
   }
 });
 
