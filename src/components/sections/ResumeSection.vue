96feae2913319f00056060964b407582c0e79e2c
avenmia <mavend2@gmail.com>
2020-11-29 11:35:43 -0600

    Refactoring Resume section to use setup instead of data

--- src/components/sections/ResumeSection.vue
+++ src/components/sections/ResumeSection.vue
@@ -8,16 +8,12 @@
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  onMounted,
-  reactive
-} from 'vue';
-import * as ResumeData from "../../assets/resumeData.json"
+import { defineComponent, onMounted, reactive } from 'vue';
 import Skills from "@/components/resume/Skills.vue"
 import WorkExperience from "@/components/resume/WorkExperience.vue"
 import Education from "@/components/resume/Education.vue"
 import Associations from "@/components/resume/Associations.vue"
+import * as ResumeData from "../../assets/resumeData.json"
 
 export default defineComponent({
   name: 'Resume',
@@ -30,33 +26,13 @@ export default defineComponent({
   setup()
   {
     const data = reactive({});
-
-    async function fetchData()
-    {
-      return await fetch("https://swapi.dev/api/people/1/", {
-        method: 'get',
-        headers: {
-          'content-type': 'application/json'
-        }
-      }).then(res => {return res.json()});
-    }
-  },
-  data()
-  {
     const {Skills, Education} = ResumeData?.Content;
     const WorkExperience = ResumeData?.Content["Work Experience"];
     const Associations = ResumeData?.Content["Professional Associations, Activities, Achievements"];
     return {
       Skills, Education, WorkExperience, Associations
     }
-  },
-  onRenderTriggered(e: any)
-  {
-    debugger;
-  },
-
-
-
+  }
 })
 </script>
 
