938f2737affc233b81423f66694b90875b48ab56
avenmia <mavend2@gmail.com>
2020-12-08 10:02:06 -0600

    Updating public path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ module.exports = {
   configureWebpack: process.env.NODE_ENV === 'production' ? {} : {
     devtool: 'source-map'
   },
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/PersonalWebsite/' : '/'
+  publicPath: '/'
 };
