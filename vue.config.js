27fe298e4bda3790945d641be741b0f4ea7229d8
avenmia <mavend2@gmail.com>
2020-12-10 20:38:58 -0600

    Updating website title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  chainWebpack: config =>
+  {
+    config
+      .plugin('html')
+      .tap(args =>
+      {
+        args[0].title = "Mike's Website";
+        return args;
+      });
+  },
   configureWebpack: process.env.NODE_ENV === 'production' ? {} : {
     devtool: 'source-map'
   },
