1bf6493ca57007768a534279ad4bd03c3e26975a
avenmia <mavend2@gmail.com>
2020-12-10 21:21:45 -0600

    fixing HomeSection test

--- src/components/__tests__/HomeSection.test.ts
+++ src/components/__tests__/HomeSection.test.ts
@@ -3,12 +3,13 @@ import { shallowMount } from "@vue/test-utils";
 
 describe("HomeSection.vue", () =>
 {
+  // Makes sure Home section is rendered correctly
   test("Renders HomeSection", () =>
   {
     const version = "v0.1";
     const wrapper = shallowMount(HomeSection, { propsData: { version } });
     expect(wrapper.html()).toContain(
-      `<h1>Michael Avendano</h1><p>Software Engineer</p><p>${version}</p>`
+      `<h1 class=\"header\"> Michael Avendano </h1><p>Software Engineer</p>`
     );
   });
 });
