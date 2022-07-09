532cd9a8f950911a04791385f519f6121a9e34ac
avenmia <mavend2@gmail.com>
2022-07-09 08:34:28 -1000

    Fixing menu bug where it wouldn't disappear based on screen size

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,44 @@ const bars = document.querySelectorAll(".bar");
 // Look into custom attributes for handling menu state
 // Add css classes that can be added or removed rather than hardcoding the styles.
 
+function setClose() {
+  if (!menu || !closeIcon) {
+    return;
+  }
+  menu.setAttribute("data-state", "close");
+  closeIcon.setAttribute("data-state", "hide");
+  Array.from(bars).forEach((bar) => {
+    bar.setAttribute("data-state", "visible");
+  });
+}
+
+function setOpen() {
+  if (!menu || !closeIcon) {
+    return;
+  }
+  menu.setAttribute("data-state", "open");
+  closeIcon.setAttribute("data-state", "visible");
+  Array.from(bars).forEach((bar) => {
+    bar.setAttribute("data-state", "hide");
+  });
+}
+
 function toggleMenu() {
-  if (!menu || !hamburger || !closeIcon) {
+  if (!menu || !closeIcon) {
     return;
   }
   // If menu is open, close the menu
   if (menu.getAttribute("data-state") === "open") {
-    menu.setAttribute("data-state", "close");
-    closeIcon.setAttribute("data-state", "hide");
-    Array.from(bars).forEach((bar) => {
-      bar.setAttribute("data-state", "visible");
-    });
+    setClose();
   } else {
-    menu.setAttribute("data-state", "open");
-    closeIcon.setAttribute("data-state", "visible");
-    Array.from(bars).forEach((bar) => {
-      bar.setAttribute("data-state", "hide");
-    });
+    setOpen();
   }
 }
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 900) {
+    setClose();
+  }
+});
 
 hamburger?.addEventListener("click", toggleMenu);
