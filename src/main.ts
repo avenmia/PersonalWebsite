ed36953845a450e0303dc8782c7de2f312432748
avenmia <mavend2@gmail.com>
2022-07-10 13:04:10 -1000

    Fix responsive issues (#130)
    * Fixing skills column count
    
    * Fixing menu bug where it wouldn't disappear based on screen size
    
    * Fixing menu responsiveness on smaller screens
    
    * Adding external link icons and fixing where the new page opens
    
    * Changing menu to a button rather than an input toggle
    
    * fixing apostraphe and okina
    
    * pinning dependencies
    
    * Removing onHover brightness change
    
    * Adding aria label to menu button

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,48 @@
-const hamburger: HTMLElement | null = document.querySelector(".toggle-label");
+const hamburger: HTMLElement | null = document.querySelector(".menu-btn");
 const menu: HTMLElement | null = document.querySelector(".mobile-navbar");
 const closeIcon: HTMLElement | null = document.querySelector("#close-icon");
 const bars = document.querySelectorAll(".bar");
 // Look into custom attributes for handling menu state
 // Add css classes that can be added or removed rather than hardcoding the styles.
 
+function setClose() {
+  if (!menu || !closeIcon) {
+    return;
+  }
+  menu.setAttribute("data-state", "close");
+  closeIcon.setAttribute("data-state", "hide");
+  Array.from(bars).forEach((bar) => {
+    bar.setAttribute("data-state", "visible");
+  });
+}
+
+function setOpen() {
+  if (!menu || !closeIcon) {
+    return;
+  }
+  menu.setAttribute("data-state", "open");
+  closeIcon.setAttribute("data-state", "visible");
+  Array.from(bars).forEach((bar) => {
+    bar.setAttribute("data-state", "hide");
+  });
+}
+
 function toggleMenu() {
-  if (!menu || !hamburger || !closeIcon) {
+  if (!menu || !closeIcon) {
     return;
   }
   // If menu is open, close the menu
   if (menu.getAttribute("data-state") === "open") {
-    menu.setAttribute("data-state", "close");
-    closeIcon.setAttribute("data-state", "hide");
-    Array.from(bars).forEach((bar) => {
-      bar.setAttribute("data-state", "visible");
-    });
+    setClose();
   } else {
-    menu.setAttribute("data-state", "open");
-    closeIcon.setAttribute("data-state", "visible");
-    Array.from(bars).forEach((bar) => {
-      bar.setAttribute("data-state", "hide");
-    });
+    setOpen();
   }
 }
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 900) {
+    setClose();
+  }
+});
 
 hamburger?.addEventListener("click", toggleMenu);
